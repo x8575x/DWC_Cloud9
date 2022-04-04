a22c48abc60272f16a225f5e92ecbc6ec653e216
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-04 04:05:58 +0000

    [Finish]2-3

---
+++ ruby/lesson3.rb
@@ -0,0 +1,13 @@
+puts "webcamp".upcase
+# upcase　小文字→大文字
+puts "WEBCAMP".downcase
+# downcase 大文字→小文字
+puts "webcamp".swapcase
+# 小文字⇔大文字
+puts "webcamp".capitalize
+# capitalize　先頭の小文字を大文字
+
+
+
+
+# 結果 WEBCAMP/webcamp/WEBCAMP/Webcamp
